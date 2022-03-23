86ac2f201ed90e8b4f5f416c83f1f392f8a068af
SpanDavid <99012395+SpanDavid@users.noreply.github.com>
2022-03-23 16:37:27 +0100

    header + footer mise en place header + footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Header from "./Header";
+import Footer from "./Footer";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <Footer />
     </div>
   );
 }
