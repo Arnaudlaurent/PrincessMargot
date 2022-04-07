3a704f7ee0215066ad61e34ab7ae5f0e1d96c9d4
SpanDavid <99012395+SpanDavid@users.noreply.github.com>
2022-04-07 16:56:50 +0200

    Ajout champs inputs, selects et textareas

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,12 +3,40 @@ import "../styles/profile.scss";
 export default function Profile() {
   return (
     <div className="profile">
-      <div>
+      <div className="input-row">
         <input type="text" defaultValue="Becquart" placeholder="Nom" />
 
         <input type="text" defaultValue="Caroline" placeholder="Prénom" />
 
-        <textarea>Texte&#13;&#10;placeholder</textarea>
+        <select>
+          <option value="majeur">Majeur</option>
+          <option value="mineur">Mineur</option>
+        </select>
+      </div>
+
+      <div className="input-row">
+        <input
+          type="text"
+          defaultValue="20/02/1988"
+          placeholder="Date de naissance"
+        />
+        <input
+          type="mail"
+          defaultValue="becquartcaroline@gmail.com"
+          placeholder="Mail"
+        />
+        <textarea rows="5" cols="30" placeholder="Adresse">
+          698 rue du Recueil, 59491 Villeneuve d'Ascq
+        </textarea>
+      </div>
+
+      <div className="input-row">
+        <select>
+          <option value="f">Fille</option>
+          <option value="m">Garçon</option>
+        </select>
+
+        <input type="text" defaultValue="Robert Debré" placeholder="Hôpital" />
       </div>
     </div>
   );
